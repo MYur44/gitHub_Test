657ebe9c6220b9ee92dbe2596753e3b78c43a268
Mustafa Yurdakul <mustafaa.yurdakul@gmail.com>
2022-04-17 22:26:41 +0100

    commit 1

--- src/Test_GITHUB.java
+++ src/Test_GITHUB.java
@@ -2,5 +2,10 @@ public class Test_GITHUB {
     public static void main(String[] args) {
         //test 1, 2, 3
 
+        //testing the features
+
+        System.out.println("---");
+
+
     }
 }
